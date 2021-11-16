42b7e1b88e734eba2c4bcc9a82e69505a3532326
Nguyen Thanh Vu <nguyenthanhvudvfb@gmail.com>
2021-11-16 07:32:44 -0800

    admin

---
+++ modules/commands/ping.js
@@ -0,0 +1,28 @@
+module.exports.config = {
+	name: "ping",
+	version: "1.0.4",
+	hasPermssion: 0,
+	credits: "Mirai Team",
+	description: "tag toàn bộ thành viên",
+	commandCategory: "system",
+	usages: "[Text]",
+	cooldowns: 5
+};
+
+module.exports.run = async function({ api, event, args }) {
+	try {
+		const botID = global.data.botID;
+		const listUserID = event.participantIDs.filter(ID => ID != botID && ID != event.senderID);
+		var body = (args.length != 0) ? args.join(" ") : "@everyone", mentions = [], index = 0;
+		
+		for(const idUser of listUserID) {
+			body = "‎" + body;
+			mentions.push({ id: idUser, tag: "‎", fromIndex: index - 1 });
+			index -= 1;
+		}
+
+		return api.sendMessage({ body, mentions }, event.threadID, event.messageID);
+
+	}
+	catch (e) { return console.log(e); }
+}
